d1afe34675a28ec3e08eaea99ec55431d3f2e99e
Jake Mangeney <jakemangeney@gmail.com>
2021-12-01 16:50:30 -0500

    got rid of unnecessary if statement in hasexactly

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -210,9 +210,7 @@ export const hasExactly = (arr, test, n) => {
     let array = [];
     let count = 0;
     for (let i = 0; i < arr.length; i++) {
-        if (i % 2 == 0) {
             array.push(test(arr[i]));
-        }
     }
     for (let i = 0; i < array.length; i++) {
         if (array[i]) {
